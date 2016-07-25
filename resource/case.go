f58d09df1dcd3e072d6f7aecf24ce9ea21ed08d9
Kevin Lo <kevin.lo@segment.com>
2016-07-25 14:15:36 -0700

    Adding custom fields support to case.

--- resource/case.go
+++ resource/case.go
@@ -5,25 +5,26 @@ import (
 )
 
 type Case struct {
-	ID              *int64     `json:"id,omitempty"`
-	ExternalID      *string    `json:"external_id,omitempty"`
-	Blurb           *string    `json:"blurb,omitempty"`
-	Subject         *string    `json:"subject,omitempty"`
-	Priority        *int       `json:"priority,omitempty"`
-	Description     *string    `json:"description,omitempty"`
-	Status          *string    `json:"status,omitempty"`
-	Type            *string    `json:"type,omitempty"`
-	Language        *string    `json:"language,omitempty"`
-	CreatedAt       *Timestamp `json:"created_at,omitempty"`
-	UpdatedAt       *Timestamp `json:"updated_at,omitempty"`
-	ChangedAt       *Timestamp `json:"changed_at,omitempty"`
-	LastActiveAt    *Timestamp `json:"active_at,omitempty"`
-	ReceivedAt      *Timestamp `json:"received_at,omitempty"`
-	LockedUntil     *Timestamp `json:"locked_until,omitempty"`
-	FirstOpenedAt   *Timestamp `json:"first_opened_at,omitempty"`
-	LastOpenedAt    *Timestamp `json:"opened_at,omitempty"`
-	FirstResolvedAt *Timestamp `json:"first_resolved_at,omitempty"`
-	LastResolvedAt  *Timestamp `json:"resolved_at,omitempty"`
+	ID              *int64                 `json:"id,omitempty"`
+	ExternalID      *string                `json:"external_id,omitempty"`
+	Blurb           *string                `json:"blurb,omitempty"`
+	Subject         *string                `json:"subject,omitempty"`
+	Priority        *int                   `json:"priority,omitempty"`
+	Description     *string                `json:"description,omitempty"`
+	Status          *string                `json:"status,omitempty"`
+	Type            *string                `json:"type,omitempty"`
+	Language        *string                `json:"language,omitempty"`
+	CreatedAt       *Timestamp             `json:"created_at,omitempty"`
+	UpdatedAt       *Timestamp             `json:"updated_at,omitempty"`
+	ChangedAt       *Timestamp             `json:"changed_at,omitempty"`
+	LastActiveAt    *Timestamp             `json:"active_at,omitempty"`
+	ReceivedAt      *Timestamp             `json:"received_at,omitempty"`
+	LockedUntil     *Timestamp             `json:"locked_until,omitempty"`
+	FirstOpenedAt   *Timestamp             `json:"first_opened_at,omitempty"`
+	LastOpenedAt    *Timestamp             `json:"opened_at,omitempty"`
+	FirstResolvedAt *Timestamp             `json:"first_resolved_at,omitempty"`
+	LastResolvedAt  *Timestamp             `json:"resolved_at,omitempty"`
+	CustomFields    map[string]interface{} `json:"custom_fields,omitempty"`
 	Resource
 }
 
